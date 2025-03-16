bda9b9a3a27d1e6b9a25ee62aa09edea1eed18fa
Byihta <hms.miranda@outlook.com>
2025-03-16 17:33:24 +0000

    Cycle duration counter

--- main.py
+++ main.py
@@ -11,12 +11,15 @@ if __name__ == '__main__':
     
     # pygame setup
     pygame.init()
+    clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     run = True
+    dt = 0
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
         screen.fill('black')
         pygame.display.flip()
+        dt += clock.tick(60) / 1000
 
